3dbedf9ef17b0cf163f008b268e3974ff2488e06
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-02-07 19:34:23 +0000

    get_size func

--- pa2_base/QueueStackBase/my_linked_list.py
+++ pa2_base/QueueStackBase/my_linked_list.py
@@ -7,17 +7,21 @@ class LinkedList():
     def __init__(self):
         self.head = Node() # the first node ----- HELO PIAZZA
         self.head = Node(10)
+
+        self.size = 0
     
-    def push_back(self, data):
+    def push_back(self, data): # O(1)
         """Takes a parameter and adds its value to the back of the list."""
         pass
+        self.size += 1
 
-    def push_front(self, data):
+    def push_front(self, data): # O(1)
         """Takes a parameter and adds its value to the front of the list."""
         pass
-    
+        self.size += 1
 
-    def pop_front(self):
+
+    def pop_front(self): # O(1)
         """Removes the item from the front of the list and returns its value."""
         return None
         # TODO: ■ If the list is empty, return None
@@ -28,16 +32,15 @@ class LinkedList():
         # TODO: ■ If the list is empty, return None
 
 
-    def get_size(self):
+    def get_size(self): # O(1)
         """Returns the number of items currently in the list."""
-        return 0
-    
+        return self.size
 
     def __str__(self):
         """Returns a string with all the items in the list, separated by a single space."""
         return_str = ""
         current = self.head
-        while current != None:
+        while current.data != None:
             return_str += f"{current.data} " # HELP TA má gera space svona
             current = current.next
         return return_str
