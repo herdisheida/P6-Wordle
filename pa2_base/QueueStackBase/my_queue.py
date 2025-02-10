622de0414d2fad251b22154b491266729ffb89c9
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-02-10 23:42:16 +0000

    make my_quququ and my_stack  prettier

--- pa2_base/QueueStackBase/my_queue.py
+++ pa2_base/QueueStackBase/my_queue.py
@@ -1,18 +1,19 @@
 from array_deque import ArrayDeque
 from my_linked_list import LinkedList
 
+
 class Queue:
     def __init__(self):
         # Pick one of these to use.
         # Stack must have the container you dont choose for Queue
-        
-        #self.container = LinkedList()
+
+        # self.container = LinkedList()
         self.container = ArrayDeque()
 
     def add(self, data):
         """Takes a parameter and adds its value to the back of the queue."""
         self.container.push_back(data)
-    
+
     def remove(self):
         """Removes the item off the front of the queue and returns its value."""
         return self.container.pop_front()
--- pa2_base/QueueStackBase/my_stack.py
+++ pa2_base/QueueStackBase/my_stack.py
@@ -1,22 +1,23 @@
 from array_deque import ArrayDeque
 from my_linked_list import LinkedList
 
+
 class Stack:
     def __init__(self):
         # Pick one of these to use.
         # Stack must have the container you dont choose for Queue
-        
+
         self.container = LinkedList()
-        #self.container = ArrayDeque()
+        # self.container = ArrayDeque()
 
     def push(self, data):
         """Takes a parameter and adds its value onto the stack."""
         self.container.push_front(data)
-    
+
     def pop(self):
         """Removes the item off the top of the stack and returns its value."""
         return self.container.pop_front()
-    
+
     def get_size(self):
         """Returns the number of items currently on the stack."""
         return self.container.get_size()
