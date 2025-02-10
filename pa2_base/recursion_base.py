3b898e7f1ef58c8cc942429374dc54613051d06b
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-02-10 20:28:43 +0000

    fínpússa

--- pa2_base/recursion_base.py
+++ pa2_base/recursion_base.py
@@ -17,113 +17,8 @@ def get_size(head):
     # inductive step
     return 1 + get_size(head.next)
 
+
 def reverse_list(head):
-    # base case
-    # if head == None or head.next == None:
-        # return head
-    # # inductive step
-    # print("head: ", head)
-    # print("head.next: ", head.next)
-    # new_head = reverse_list(head.next)
-    # print("new_head: ",new_head)
-    # head.next = head
-    # head = None
-    # print("head: ", head.data)
-    # print("head.next: ", head.next.data if head.next != None else 124)
-
-    # LOOP forever
-    # new_head = Node(head.next, head)
-    # return reverse_list(new_head)
-
-
-    # test ----- GET THE SAME AS LIST...
-    # node = reverse_list(head.next)
-    # return Node(head.data, node)
-
-    # node = reverse_list(head.next)
-    # print("node: ", node)
-    # if node == None or node.next == None or node.next.next == None:
-    #     return head
-    # return Node(node.data, node.next)
-
-    # node = reverse_list(head.next)
-    # print("node: ", node)
-    # if head == None or node == None or node.next == None or node.next.next == None:
-    #     return head
-    # return Node(node.next.data, node)
-
-    # node = reverse_list(head.next)
-    # print("node: ", node)
-    # if head == None or node == None:
-    #     return head
-    # return Node(node.data, node)
-
-
-    # the front due is correct, otherwise not
-    # node = reverse_list(head.next)
-    # if node == None:
-    #     return head
-    # return Node(node.data, head)
-
-    # node = Node(head.next, head)
-    # if node == None:
-    #     return head
-    # return reverse_list(node)
-
-    # not working
-    # node = reverse_list(head.next)
-    # if node == None:
-    #     return head
-    # return Node(node, head.next)
-
-
-
-
-    # WORKS but is time is not good :((
-    # reversed = reverse_list(head.next)
-    # current = reversed
-    # while current.next:
-    #     current = current.next
-    # current.next = Node(head.data)
-    # return reversed
-
-
-
-    # def reverse_inner(head, reversed=None):
-    #     if head == None:
-    #         return reversed
-    #     if head.next == None:
-    #         head.next = reversed
-    #         return head
-        
-    # if head == None or head.next == None:
-    #     return head
-    
-    # print("before", head.data)
-    # reverse_list(head.next)
-    # print("after:", head.data)
-    # # print()
-    # # node = Node(head.data, head)
-    # # print("NODE: ", node.data)
-
-    # def reverse_inner(head, tail=None):
-    #     # base case
-    #     if head == None or head.next == None:
-    #         return head
-    #     if tail == None:
-    #         return 
-        
-    #     # inductive step
-    #     new_head = Node(head.next.data, head.next.next)
-    #     new_tail = Node(head.data, head.next)
-    #     return reverse_inner(new_head, new_tail)
-
-
-    # return reverse_inner(head)
-        
-
-
-
     def reverse_inner(node):
         # base case
         if not node:
@@ -133,10 +28,10 @@ def reverse_list(head):
             return head, tail
         
         # inductive step
-        rev_head, rev_tail = reverse_inner(node.next) # {rev_head, ..., rev_tail}
-        rev_tail.next = Node(node.data) # {rev_head, ..., rev_tail, rev_tail.next}
-        return rev_head, rev_tail.next # {returning new_head, ...,  new_tail}
-    
+        new_head, new_tail = reverse_inner(node.next)   # (new_head) -> ... -> (new_tail)
+        new_tail.next = Node(node.data)                 # (new_head) -> ... -> (new_tail) -> (new_tail.next)
+        return new_head, new_tail.next                  # returning (new_head) -> ... -> (new_tail.next)
+
     new_head, x = reverse_inner(head)
     return new_head
 
