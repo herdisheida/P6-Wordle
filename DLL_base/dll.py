d5eba1af3080481c714998f51d9848906eebd7fc
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-02-20 16:32:06 +0000

    make moveos shorter

--- DLL_base/dll.py
+++ DLL_base/dll.py
@@ -18,11 +18,6 @@ class DLL:
     def insert(self, data):
         """Inserts an item with that value in front of the node at the current position"""
         node = Node(data, self.current_pos.prev, self.current_pos)
-        # JB - þarf held ekki - það gerist nákvæmlega það sama
-        #  if self.size == 0:
-        #     self.head.next = node
-        #     self.tail.prev = node
-        # else:
         self.current_pos.prev.next = node # connect left to newNode (left->new)
         self.current_pos.prev = node # connect right to newNode (new<-right)
         self.current_pos = node
@@ -31,7 +26,7 @@ class DLL:
         
     def remove(self):
         """Removes the node at the current position if there is one (otherwise does nothing)"""
-        if self.current_pos != self.tail and self.current_pos:
+        if self.current_pos != self.tail: # and self.current_pos is not None:
             self.current_pos.prev.next = self.current_pos.next ## connect left  to right
             self.current_pos.next.prev = self.current_pos.prev ## connect right to left
             self.current_pos = self.current_pos.next
@@ -46,7 +41,7 @@ class DLL:
     def move_to_next(self):
         """Moves the current position one item closer to the tail/trailer.
         Do nothing if at end"""
-        if self.current_pos != self.tail and self.current_pos: # CHECK the if-statement
+        if self.current_pos != self.tail: # and self.current_pos is not None: # CHECK the if-statement
             self.current_pos = self.current_pos.next
 
     def move_to_prev(self):
@@ -55,17 +50,23 @@ class DLL:
         if self.current_pos.prev != self.head:
             self.current_pos = self.current_pos.prev
 
-    def move_to_pos(self, pos): # JB - pretty?
+    def move_to_pos(self, pos):
         """Moves the current position to item #position in the list.
         The first actual data item is #0"""
-        if (self.size != 0) and (0 <= pos < self.size):
-            index = 0
-            self.current_pos = self.head.next
-            while index != pos: 
-                self.current_pos = self.current_pos.next
-                index += 1
+        # if (self.size != 0) and (0 <= pos < self.size):
+        #     index = 0
+        #     self.current_pos = self.head.next
+        #     while index != pos: 
+        #         self.current_pos = self.current_pos.next
+        #         index += 1
 
-    def clear(self): # JB - dunno if right HELP
+        if pos < 0 or pos >= self.size:
+            return
+        self.current_pos = self.head.next
+        for x in range(pos):
+            self.move_to_next()
+
+    def clear(self): # HELP
         """Clears all nodes from the list"""
         self.head.next = self.tail
         self.tail.prev = self.head
@@ -83,30 +84,48 @@ class DLL:
         if self.size == 0:
             return None
         return self.tail.prev
-    
 
 
-
-
-    def partition(self, low, high):
+    def partition(self, low, high): #  JB CHECK -- base case
         """Loops from low to high and moves all nodes smaller than low so they are ahead (left side) of the low node"""
-        # JB CHECK -- base case
         if low == high or self.size == 0:
             return
         
-        temp_node = low
-        while temp_node != high.next:
+
+        # pivot = low
+        # low_data = low.data
+        # while pivot != high.next:
+        #     # check if "temp" is smaller than "low"
+        #     if pivot.data < low_data:
+
+        #         temp_data = pivot.data
+        #         pivot.data = low_data
+        #         pivot.prev.data = temp_data
+
+        #     pivot = pivot.next
+        # self.current_pos = low
+        # return self.current_pos
+    
+        
+        pivot = low
+        while pivot != high.next:
             # check if "temp" is smaller than "low"
-            if temp_node.data < low.data:
+            if pivot.data < low.data:
                 # put the temp node to in front of "low"
                 self.current_pos = low
-                self.insert(temp_node.data)
+                self.insert(pivot.data)
                 # severe the temp node from the original position
-                self.current_pos = temp_node
+                self.current_pos = pivot
                 self.remove()
-            temp_node = temp_node.next
+            pivot = pivot.next
         self.current_pos = low
         return self.current_pos
+
+
+
+
+
+
 
 
     def sort(self): # TODO
@@ -116,16 +135,14 @@ class DLL:
         high = self.get_last_node()
             
         self.quick_sort(low, high)
-        self.current_pos = low
 
     def quick_sort(self, low, high):
         
         # base case
         if (low and high) and (low != high) and (low != high.next):
 
-            # recursive
+            # inductive step
             pivot = self.partition(low, high)
-
             # recursivly sort left side of "low"
             self.quick_sort(low, pivot.prev)
             # recursively sort right side of "low"
@@ -150,24 +167,29 @@ class DLL:
 
 if __name__ == "__main__":
     # create tests here if you want
-    dll = DLL()
+    # dll = DLL()
 
     # empty sort
-    dll.sort()
-    assert str(dll) == ""
+    # dll.sort()
+    # assert str(dll) == ""
+    # assert dll.head.next == dll.tail
+    # assert dll.tail.prev == dll.head
+    # assert dll.size == 0
+    # assert dll.current_pos == dll.tail
 
-    dll.insert(1)
-    dll.insert(6)
-    dll.insert(4)
-    dll.insert(3)
-    dll.insert(5)
+    # dll.insert(1)
+    # dll.insert(6)
+    # dll.insert(4)
+    # dll.insert(3)
+    # dll.insert(5)
 
-    # noraml sort
-    dll.sort()
-    assert str(dll) == "1 3 4 5 6 "
+    # # noraml sort
+    # dll.sort()
+    # assert str(dll) == "1 3 4 5 6 "
 
-    # array with 1 node sort
-    dll = DLL()
-    dll.insert(1)
-    dll.sort()
-    assert str(dll) == "1 "
+    # # array with 1 node sort
+    # dll = DLL()
+    # dll.insert(1)
+    # dll.sort()
+    # assert str(dll) == "1 "
+    pass
