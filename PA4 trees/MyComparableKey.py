9268c57d92e03c0ae49461a691af79c19689eb0e
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-03-04 17:29:02 +0000

    create class MyComparableKey

--- PA4 trees/MyComparableKey.py
+++ PA4 trees/MyComparableKey.py
@@ -3,9 +3,16 @@ class MyComparableKey:
 
     def __init__(self, int_value, string_value):
         """ A constructor that takes an integer value and a string value """
-        pass
+        self.int_val = int_value
+        self.str_val = string_value
 
     def __lt__(self, other):
-        """ Compares two instances of MyComparableKey and returns True if the value of self is lower, otherwise False. """
-        pass
+        """ Compares two instances of MyComparableKey and returns True if the value of self is lower, otherwise False. 
+         In case of equal integers the order of the strings is used """
+        if self.int_val == other.int_val:
+            if self.str_val < other.str_val:
+                return True
+        if self.int_val < other.int_val:
+            return True    
+        return False
 
