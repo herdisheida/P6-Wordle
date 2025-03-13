29140e558c6b024f9dc21d3a25281c004ba78407
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-03-13 21:15:30 +0000

    fix comments and docstring

--- skil/Bucket.py
+++ skil/Bucket.py
@@ -1,5 +1,7 @@
 class ItemExistsException(Exception):
     pass
+
+
 class NotFoundException(Exception):
     pass
 
@@ -12,14 +14,14 @@ class Node:
 
 
 class Bucket:
-    """ Implement the class Bucket with a singly-linked list """
+    """Implement the class Bucket with a singly-linked list"""
 
     def __init__(self):
         self.head = None
         self.size = 0
 
     def insert(self, key, data):
-        """ Adds this value pair to the collection """
+        """Adds this value pair to the collection"""
         if self.contains(key):
             raise ItemExistsException()
 
@@ -32,14 +34,14 @@ class Bucket:
         self.size += 1
 
     def update(self, key, data):
-        """ Sets the data value of the value pair with equal key to data """
+        """Sets the data value of the value pair with equal key to data"""
         node = self._get_node(key)
         node.data = data
 
     def find(self, key):
-        """ Returns the data value of the value pair with equal key """
+        """Returns the data value of the value pair with equal key"""
         return self._get_node(key).data
-            
+
     def _get_node(self, key):
         curr = self.head
         while curr:
@@ -47,9 +49,9 @@ class Bucket:
                 return curr
             curr = curr.next
         raise NotFoundException()
-        
+
     def contains(self, key):
-        """ Returns True if equal key is found in the collection, otherwise False """
+        """Returns True if equal key is found in the collection, otherwise False"""
         try:
             self._get_node(key)
             return True
@@ -57,7 +59,7 @@ class Bucket:
             return False
 
     def remove(self, key):
-        """ Removes the value pair with equal key from the collection """
+        """Removes the value pair with equal key from the collection"""
         if self.head == None:
             raise NotFoundException()
 
@@ -65,37 +67,34 @@ class Bucket:
             self.head = self.head.next
             self.size -= 1
             return
-        
+
         curr = self.head
         while curr.next:
-
             if curr.next.key == key:
                 curr.next = curr.next.next
                 self.size -= 1
                 return
             curr = curr.next
         raise NotFoundException()
-    
 
     def __setitem__(self, key, data):
-        """ Override to allow this syntax: some_hash_map[key] = data """
+        """Override to allow this syntax: some_hash_map[key] = data"""
         try:
             self.update(key, data)
         except NotFoundException:
             self.insert(key, data)
 
     def __getitem__(self, key):
-        """ Override to allow this syntax: my_data = some_bucket[key] """
+        """Override to allow this syntax: my_data = some_bucket[key]"""
         return self.find(key)
 
     def __len__(self):
-        """ Override to allow this syntax: length_of_structure = len(some_bucket) """
+        """Override to allow this syntax: length_of_structure = len(some_bucket)"""
         return self.size
-    
 
     # DELETE - __str__ fall er ekki í verkefnalýsingunni
     def __str__(self):
-        """ testing BUCKET str """
+        """testing BUCKET str"""
         return_str = ""
         current = self.head
         while current:
--- skil/HashMap.py
+++ skil/HashMap.py
@@ -1,84 +1,84 @@
 # TODO -- muna að búa til hash fall :D :D :D
 # TODO -- reyna gera fall fyrir að finna index -- alltaf sami hluturinn
 
-from Bucket import Bucket
+from skil.Bucket import Bucket # LATER - breyta import - eyða "skil."
+
 
 class HashMap:
 
     def __init__(self):
-        self.item_count = 0 # num of items in the entire data structure
-        self.lis_size = 8 # # num of buckets
+        self.item_count = 0      # num of items in the entire data structure
+        self.bucket_count = 8    # num of buckets (capacity)
 
-        self.bucket_list = [Bucket() for _ in range(self.lis_size)]
- 
+        self.bucket_list = [Bucket() for _ in range(self.bucket_count)]
+
     def insert(self, key, data):
-        """ Adds this value pair to the collection """
+        """Adds this value pair to the collection"""
         self.rebuild()
-        index = self._compression(key)
+        index = self._get_bucket_index(key)
         self.bucket_list[index].insert(key, data)
         self.item_count += 1
 
     def update(self, key, data):
-        """ Sets the data value of the value pair with equal key to data """
-        index = self._compression(key)
+        """Sets the data value of the value pair with equal key to data"""
+        index = self._get_bucket_index(key)
         self.bucket_list[index].update(key, data)
 
     def find(self, key):
-        """ Returns the data value of the value pair with equal key """
-        index = self._compression(key)
+        """Returns the data value of the value pair with equal key"""
+        index = self._get_bucket_index(key)
         return self.bucket_list[index].find(key)
 
     def contains(self, key):
-        """ Returns True if equal key is found in the collection, otherwise False """
-        index = self._compression(key)
+        """Returns True if equal key is found in the collection, otherwise False"""
+        index = self._get_bucket_index(key)
         return self.bucket_list[index].contains(key)
-    
+
     def remove(self, key):
-        """ Removes the value pair with equal key from the collection """
-        index = self._compression(key)
+        """Removes the value pair with equal key from the collection"""
+        index = self._get_bucket_index(key)
         self.bucket_list[index].remove(key)
         self.item_count -= 1
-    
+
     def __setitem__(self, key, data):
-        """ Override to allow this syntax: some_hash_map[key] = data """
-        index = self._compression(key)
+        """Override to allow this syntax: some_hash_map[key] = data"""
+        index = self._get_bucket_index(key)
         if not self.contains(key):
             self.item_count += 1
         self.bucket_list[index][key] = data
 
     def __getitem__(self, key):
-        """ Override to allow this syntax: my_data = some_hash_map[key """
-        index = self._compression(key)
+        """Override to allow this syntax: my_data = some_hash_map[key"""
+        index = self._get_bucket_index(key)
         return self.bucket_list[index][key]
 
     def __len__(self):
-        """ Returns the number of items in the entire data structure """
+        """Returns the number of items in the entire data structure"""
         return self.item_count
-    
-    def rebuild(self):
-        """ When the number of items in the HashMap has reached 120% of the number of buckets
-         (length of array or list), it must doubling the number of buckets """
-        if self.lis_size * 1.2 >= self.item_count:
 
-            # initalize bucket list
-            temp = self.bucket_list
-            self.lis_size *= 2
-            self.bucket_list = [Bucket() for _ in range(self.lis_size)]
-            
-            # re allocate items
-            for x in range(len(temp)):
-                node = temp[x].head
+
+    def rebuild(self):
+        """When the num of items in the HashMap has reached 120% of the num of buckets.
+        Double the bucket_list_size and redistributes all key-value pairs"""
+        if self.item_count >= self.bucket_count * 1.2:
+            old_bucket_list = self.bucket_list
+            self.bucket_count *= 2
+            self.bucket_list = [Bucket() for _ in range(self.bucket_count)]
+            self.item_count = 0 # reset before redistributing
+
+            for bucket in old_bucket_list:
+                node = bucket.head
                 while node:
                     self.insert(node.key, node.data)
                     node = node.next
 
-    def _compression(self, key):
-        """ Get index """
-        return hash(key) % self.lis_size
+    def _get_bucket_index(self, key):
+        """Get index"""
+        return hash(key) % self.bucket_count
 
     def __str__(self):
-        """ testing HASH MAP str """
-    # DELETE later - þessi klasi hefur ekki __str__ func
+        """testing HASH MAP str"""
+        # DELETE - þessi klasi hefur ekki __str__ func
         ret = ""
         for x in self.bucket_list:
             ret += str(x)
